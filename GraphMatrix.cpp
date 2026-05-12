392e30a630c45a5d41171c2b606396161605b003
Krishang Naikar <krishangnaikar7@gmail.com>
2026-05-11 20:07:22 -0700

    sub 1

--- GraphMatrix.cpp
+++ GraphMatrix.cpp
@@ -1,15 +1,31 @@
 #include "GraphMatrix.hpp"
 
-GraphMatrix::GraphMatrix(int numV, GType type) {
-    // TODO
+GraphMatrix::GraphMatrix(int numV, GType type)
+    : Graph(numV, 0, type) {
+    // Allocate 2D adjacency matrix and initialize all entries to false
+    adjMatrix = new bool*[numV];
+    for (int i = 0; i < numV; i++) {
+        adjMatrix[i] = new bool[numV];
+        for (int j = 0; j < numV; j++) {
+            adjMatrix[i][j] = false;
+        }
+    }
 }
 
 GraphMatrix::~GraphMatrix() {
-    // TODO
+    // Release each row, then the array of row pointers
+    for (int i = 0; i < v; i++) {
+        delete[] adjMatrix[i];
+    }
+    delete[] adjMatrix;
 }
 
 bool GraphMatrix::isEdge(int i, int j) const {
-    // TODO
+    // Validate vertex indices
+    if (i < 0 || i >= v || j < 0 || j >= v) {
+        return false;
+    }
+    return adjMatrix[i][j];
 }
 
 ostream& GraphMatrix::print(ostream& os) const {
@@ -37,5 +53,20 @@ ostream& GraphMatrix::print(ostream& os) const {
 }
 
 void GraphMatrix::toggleEdge(int i, int j) {
-    // TODO
+    // Validate vertex indices
+    if (i < 0 || i >= v || j < 0 || j >= v) {
+        return;
+    }
+
+    if (graphType == UNDIRECTED) {
+        // Toggle both directions symmetrically
+        adjMatrix[i][j] = !adjMatrix[i][j];
+        adjMatrix[j][i] =  adjMatrix[i][j];
+        e += adjMatrix[i][j] ? 1 : -1;
+    }
+    else {
+        // Directed: toggle only the i→j direction
+        adjMatrix[i][j] = !adjMatrix[i][j];
+        e += adjMatrix[i][j] ? 1 : -1;
+    }
 }
